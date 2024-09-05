f311864f296949d5dc1ca74dd59c432ec2b361d1
hunh17 <victoor17090@gmail.com>
2024-09-05 11:35:38 -0300

    api pronta

---
+++ api.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/api/livros', methods=['GET'])
+def get_livros():
+    livros = Livro.query.all()
+    return jsonify([{
+        "id":livro.id,
+        "titulo": livro.titulo,
+        "autor": livro.autor
+    } for livro in livros])
+
+@app.route('/api/livros/<int:id>', methods=['GET'])
+def get_livro(id):
+    livro = Livro.query.get(id)
+    
+    return jsonify({
+        "id":livro.id,
+        "titulo": livro.titulo,
+        "autor": livro.autor
+    }) if livro else ('', 404)
+
+@app.route('/api/livros', methods=['POST'])
+def add_livro():
+    dados = request.get_json()
+    novo_livro = Livro(titulo=dados['titulo'], autor=dados['autor'])
+    db.session.add(novo_livro)
+    db.session.commit()
+    return jsonify({
+        "id":novo_livro.id,
+        "titulo": novo_livro.titulo,
+        "autor": novo_livro.autor
+    }), 201
+
+@app.route('/api/livros/<int:id>', methods=['DELETE'])
+def delete_livro(id):
+    livro = Livro.query.get(id)
+
+    if livro is None:
+
+        return jsonify({"error":"livro nao encontrado"}), 404
+    db.session.delete(livro)
+    db.session.commit()
+    return '', 204
+
+@app.route('/api/livros/<int:id>', methods=['put'])
+def update_livro(id):
+    livro = Livro.query.get(id)
+
+
+    if livro is None:
+
+        return jsonify({"error":"livro nao encontrado"}), 404
+    dados = request.get_json()
+    livro.titulo = dados.get('titulo', livro.titulo)
+    livro.autor = dados.get('autor', livro.autor)
+
+    db.session.commit()
+
+    return jsonify({
+        "id":livro.id,
+        "titulo":livro.titulo,
+        "autor":livro.autor,
+    }), 201
+
+
+
+
+if __name__ == '__main__': 
+    app.run(port=5000, debug=True)
